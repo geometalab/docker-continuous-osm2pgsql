6af5c0678c84123d9aa2e6ddc7ccf44f7198e2f9
Nicola Jordan <nic@hixi.ch>
2016-03-21 18:47:38 +0100

    merging should be simpler, but currently is not

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build -f osm2pqsgl.dockerfile -t geometalab/osm2pgsql:latest .
+docker build -t geometalab/osm2pgsql:latest .
 docker push geometalab/osm2pgsql:latest
--- update_db.sh
+++ update_db.sh
@@ -11,5 +11,6 @@ osmupdate -v /var/data/osm-planet/pbf/planet-latest.osm.pbf /var/data/osm-planet
 # osm2pgsql --append -keep-coastlines \
 #   -H database -U gis -d gis --slim -C 30000 \
 #   --flat-nodes /var/cache/osm-cache /var/data/osm-planet/pbf/planet-latest.osc.gz && \
-osmconvert -v /var/data/osm-planet/pbf/planet-latest.osm.pbf /var/data/osm-planet/pbf/planet-latest.osc.gz -o=/var/data/osm-planet/pbf/planet-latest.osm.pbf &&
-rm -f /var/data/osm-planet/pbf/planet-latest.osc.gz
+mv /var/data/osm-planet/pbf/planet-latest.osm.pbf /var/data/osm-planet/pbf/planet-old.osm.pbf &&
+osmconvert -v /var/data/osm-planet/pbf/planet-old.osm.pbf /var/data/osm-planet/pbf/planet-latest.osc.gz -o=/var/data/osm-planet/pbf/planet-latest.osm.pbf &&
+rm -f /var/data/osm-planet/pbf/planet-old.osm.pbf /var/data/osm-planet/pbf/planet-latest.osc.gz

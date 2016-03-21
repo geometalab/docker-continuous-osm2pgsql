680f2849e8696f7e9f13798be8cad253ca49f81f
Nicola Jordan <nic@hixi.ch>
2016-03-21 18:54:31 +0100

    remove dead code

--- update_db.sh
+++ update_db.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-# WORKDIR_OSM=/var/data/osm-planet/update
-# mkdir -p ${WORKDIR_OSM}
-# osmosis/bin/osmosis --read-replication-interval-init workingDirectory=${WORKDIR_OSM}
-# osmosis/bin/osmosis --read-replication-interval workingDirectory=${WORKDIR_OSM} --simplify-change \
-#  --write-xml-change - | osm2pgsql --append --keep-coastlines \
-#    -H database -U gis -d gis --slim -C 30000 \
-#    --flat-nodes /var/cache/osm-cache -
-
 osmupdate -v /var/data/osm-planet/pbf/planet-latest.osm.pbf /var/data/osm-planet/pbf/planet-latest.osc.gz && \
 # osm2pgsql --append -keep-coastlines \
 #   -H database -U gis -d gis --slim -C 30000 \

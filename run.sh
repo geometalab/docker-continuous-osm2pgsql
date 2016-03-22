8a2f5470068070c6e61d694dc413249c1612dd6b
Raphael Das Gupta <raphael.das.gupta@hsr.ch>
2016-03-22 14:13:26 +0100

    refactoring: extract variable for PBF directory

--- run.sh
+++ run.sh
@@ -3,11 +3,12 @@ set -e
 nice="nice -n 19"
 osm2pgsql_base_args="--number-processes 8 --keep-coastlines -H database -U gis -d gis --slim -C 30000 --flat-nodes /var/data/osm-cache"
 osm_planet_base_path="/var/data/osm-planet"
-planet_latest="${osm_planet_base_path}/pbf/planet-latest.osm.pbf"
-planet_diff="${osm_planet_base_path}/pbf/planet-latest.osc.gz"
+pbf_dir="${osm_planet_base_path}/pbf"
+planet_latest="${pbf_dir}/planet-latest.osm.pbf"
+planet_diff="${pbf_dir}/planet-latest.osc.gz"
 
 initial_import() {
-  mkdir -p ${osm_planet_base_path}/pbf
+  mkdir -p ${pbf_dir}
   ${nice} wget -O ${planet_latest} http://ftp5.gwdg.de/pub/misc/openstreetmap/planet.openstreetmap.org/pbf/planet-latest.osm.pbf
   ${nice} osm2pgsql -c "${osm2pgsql_base_args}" ${planet_latest}
 }
@@ -15,9 +16,9 @@ initial_import() {
 update() {
   ${nice} osmupdate -v ${planet_latest} ${planet_diff}
   ${nice} osm2pgsql --append "${osm2pgsql_base_args}" ${planet_diff}
-  ${nice} osmconvert -v ${planet_latest} ${planet_diff} -o=${osm_planet_base_path}/pbf/planet-latest-new.osm.pbf
+  ${nice} osmconvert -v ${planet_latest} ${planet_diff} -o=${pbf_dir}/planet-latest-new.osm.pbf
   # TODO: maybe shutdown workers
-  ${nice} mv ${osm_planet_base_path}/pbf/planet-latest-new.osm.pbf ${planet_latest}
+  ${nice} mv ${pbf_dir}/planet-latest-new.osm.pbf ${planet_latest}
   # TODO: maybe startup workers
   ${nice} rm -f ${planet_diff}
 }
